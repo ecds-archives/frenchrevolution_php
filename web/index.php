a9d71835a96e3e57fdf40f5157f35cd870af4ae8
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2006-08-08 23:31:55 +0000

    simplified front page design with css

---
+++ web/index.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+   <title>French Revolution Pamphlet Collection</title>
+   <link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+
+<div id="index">
+
+   
+   <p id="overview" class="link"><a href="overview.php"><img src="images/select-off.gif"> Overview</a></p>
+   <p id="list" class="link"><a href="list.php"><img src="images/select-off.gif"> Pamphlet List</a></p>
+   <p id="guide" class="link"><a href="revguide.php"><img src="images/select-off.gif"> Research Guide</a></p> 
+
+   
+    <p id="title">French Revolution</p>
+    <p id="page">Pamphlet Collection</p>
+
+   
+</div>
+
+<div id="search">
+<form action="search.php" method="get">
+   Keyword search: <input type="text" name="kw"/>
+   <input type="submit" value="Go"/>
+   </form>
+</div>
+
+
+<?php
+include("footer.xml");
+?>
+
+</body>
+</html>
