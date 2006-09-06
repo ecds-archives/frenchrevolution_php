7b08da667216c1a9ea431256cf54bd005e1879e4
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2006-09-06 17:22:42 +0000

    exist db config settings

---
+++ web/config.php
@@ -0,0 +1,17 @@
+<?php
+
+/* Configuration settings for entire site */
+
+/* exist settings  */
+$server = "wilson.library.emory.edu";
+$port = "8080";
+$db = "frenchrevolution";
+
+$exist_args = array('host'   => $server,
+	      	    'port'   => $port,
+		    'db'     => $db,
+		    'dbtype' => "exist");
+
+
+
+?>
