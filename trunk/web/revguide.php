8e6aba5b48bbf1752600a2bddc30ca8a4789c274
rsutton <rsutton@1a92f4bb-5a3a-0410-90c0-e89051d5aa4e>
2006-09-18 16:20:25 +0000

    cleaned up word html to make fonts consistent throughout site

--- trunk/web/revguide.php
+++ trunk/web/revguide.php
@@ -10,132 +10,95 @@ $headtitle = "Research Guide";
 include("header.php");
 ?>
 
-<p class=MsoNormal><span style='font-size:12.0pt'><b><font face="Arial, Helvetica, sans-serif">RESEARCH 
-        GUIDE</font></b></span><font face="Arial, Helvetica, sans-serif"><span
-style='font-size:12.0pt'>: This contains information related to the bulk of the 
+<p><b>RESEARCH  GUIDE</b>: This contains information related to the bulk of the 
         collection held by Special Collections as well as additional information 
-        provided with the aim of furthering scholarship.<o:p></o:p></span></font></p>
-      <p class=MsoNormal><font face="Arial, Helvetica, sans-serif"><span style='font-size:12.0pt'> 
-        <![if !supportEmptyParas]>
-        &nbsp; 
-        <![endif]>
-        <o:p></o:p></span></font></p>
-      <p class=MsoNormal><font face="Arial, Helvetica, sans-serif"><span style='font-size:12.0pt'><a href="#coll">The 
-        Collection</a><o:p></o:p></span></font></p>
-      <p class=MsoNormal><font face="Arial, Helvetica, sans-serif"><span style='font-size:12.0pt'><a href="#prov">Provenance</a><o:p></o:p></span></font></p>
-      <p class=MsoNormal><font face="Arial, Helvetica, sans-serif"><span style='font-size:12.0pt'><a href="#link">Links</a><o:p></o:p></span></font></p>
-      <p class=MsoNormal><font face="Arial, Helvetica, sans-serif"><span style='font-size:12.0pt'><a href="http://chaucer.library.emory.edu/frenchrevo/biblioguide.html">Bibliography<o:p></o:p></a><o:p></o:p></span></font></p>
-      <p class=MsoNormal><font face="Arial, Helvetica, sans-serif"><span style='font-size:12.0pt'> 
-        <![if !supportEmptyParas]>
-        &nbsp; 
-        <![endif]>
-        <o:p></o:p><o:p></o:p></span></font></p>
-      <p class=MsoNormal><font face="Arial, Helvetica, sans-serif"><span style='font-size:12.0pt'><b><a name="coll"></a>Special 
-        Collections</b>: The French Revolution Pamphlet Collection<o:p></o:p><o:p></o:p></span></font></p>
-      <p class=MsoNormal><font face="Arial, Helvetica, sans-serif"><span style='font-size:12.0pt'>The 
-        publication dates of the pamphlets range from 1776-1817, although the 
-        bulk dates from 1789-1795.<span
-style="mso-spacerun: yes">&nbsp; </span>The collection consists of some 3000 official 
+        provided with the aim of furthering scholarship.</p>
+
+      <p><a href="#coll">The Collection</a></p>
+      <p><a href="#prov">Provenance</a></p>
+      <p><a href="#link">Links</a></p>
+      <p><a href="biblioguide.php">Bibliography</p>
+
+<p><b><a name="coll"></a>Special Collections</b>: The French Revolution Pamphlet Collection</p>
+
+<p>The publication dates of the pamphlets range from 1776-1817, although the 
+bulk dates from 1789-1795. The collection consists of some 3000 official 
         reports, speeches, letters, documents, laws, and literary publications, 
-        cataloged in three main series as detailed below.<o:p></o:p><o:p></o:p></span></font></p>
-      <p class=MsoNormal style='margin-right:32.0pt;tab-stops:right 525.45pt'><font face="Arial, Helvetica, sans-serif"><span
-style='font-size:12.0pt'>French Revolution Pamphlets I.<br>
-        Euclid Record:<span style="mso-spacerun: yes">&nbsp; </span>DC 137.08 
-        A2 <o:p></o:p><br>
+        cataloged in three main series as detailed below.</p>
+
+     <p>French Revolution Pamphlets I.<br>
+        Euclid Record:DC 137.08 A2 <br>
         Please see the digital version of <a href="vols1-3.php">Walter 
-        Robert Brown's Bibliography</a> of these pamphlets.<o:p></o:p><br>
+        Robert Brown's Bibliography</a> of these pamphlets.<br>
+
         377 pamphlets in 11 vols., mainly speeches relating to the trial of Louis 
-        XVI before the French National Convention, Paris, 1792-1793. <o:p></o:p> 
+        XVI before the French National Convention, Paris, 1792-1793. 
         [Card catalogue &quot;tracing tray&quot; lists them by vol. #, legislative 
-        # and contains only the authors' last names.<span
-style="mso-spacerun: yes">&nbsp; </span>Vols. VII-XII lack legis. #s, and catalogue 
-        doesn't list vols. XII and XIII.<span style="mso-spacerun:
-yes">&nbsp; </span>These two latter are listed in Robert Brown's biblio (DC137.08.B7 
-        in SpecColl. and see link above).<span style="mso-spacerun:
-yes">&nbsp; </span>Brown lists the collection by legis. #, and includes the authors' 
-        last names as well as their departement.]<o:p></o:p><o:p></o:p></span></font></p>
-      <p class=MsoNormal><font face="Arial, Helvetica, sans-serif"><span style='font-size:12.0pt'>French 
-        Revolution Pamphlets II.<o:p></o:p><br>
-        Euclid Record: DC141 Al nos. 1-891<o:p></o:p><br>
+        # and contains only the authors' last names.
+	Vols. VII-XII lack legis. #s, and catalogue 
+        doesn't list vols. XII and XIII.
+	These two latter are listed in Robert Brown's biblio (DC137.08.B7 
+        in SpecColl. and see link above).
+        Brown lists the collection by legis. #, and includes the authors' 
+        last names as well as their departement.]</p>
+	
+      <p>French Revolution Pamphlets II.<br>
+        Euclid Record: DC141 Al nos. 1-891<br>
         891 pamphlets containing a varied collection of addresses, letters, and 
         essays by political, religious, and literary figures, arranged alphabetically 
-        by author or originating agency.<o:p></o:p></span></font></p>
-      <p class=MsoNormal><font face="Arial, Helvetica, sans-serif"><span
-style='font-size:12.0pt'>French Revolution Pamphlets III.<span
-style="mso-spacerun: yes">&nbsp; </span><o:p></o:p><br>
-        Euclid Record:<span style="mso-spacerun: yes">&nbsp; </span>DC141 All 
-        v. 1-119<o:p></o:p><br>
-        1,710 pamphlets, bound in 119 volumes, are organized as follows: <o:p></o:p><br>
+        by author or originating agency.</p>
+	
+      <p>French Revolution Pamphlets III.<br>
+        Euclid Record: DC141 All v. 1-119<br>
+        1,710 pamphlets, bound in 119 volumes, are organized as follows: <br>
         --Speeches and miscellaneous political pamphlets (905 pamphlets in 69 
-        vols., v. 1-69)<o:p></o:p><br>
+        vols., v. 1-69)<br>
         --Reports of the Assembl&eacute;e Nationale (333 pamphlets in 17 vols., 
-        v. 70-86)<o:p></o:p><br>
-        --Reports of the Convention Nationale (129 pamphlets in 11 vols., v. 87-97)<o:p></o:p><br>
-        --Reports of the Corps Legislatif (20 pamphlets in 1 vol., v. 98)<o:p></o:p><br>
-        --Reports of the Directoire Executif (17 pamphlets in 1 vol., v. 99)<o:p></o:p><br>
-        --Laws, Statutes, etc. (22 pamphlets in 2 vols., v. 100-101)<o:p></o:p><br>
-        --Administrative Reports (23 pamphlets in 1 vol., v. 102)<o:p></o:p><br>
-        --Municipal pamphlets, Aix - Ville (100 pamphlets in 4 vols., v. 103-106)<b><o:p></o:p><br>
+        v. 70-86)<br>
+        --Reports of the Convention Nationale (129 pamphlets in 11 vols., v. 87-97)<br>
+        --Reports of the Corps Legislatif (20 pamphlets in 1 vol., v. 98)<br>
+        --Reports of the Directoire Executif (17 pamphlets in 1 vol., v. 99)<br>
+        --Laws, Statutes, etc. (22 pamphlets in 2 vols., v. 100-101)<br>
+        --Administrative Reports (23 pamphlets in 1 vol., v. 102)<br>
+        --Municipal pamphlets, Aix - Ville (100 pamphlets in 4 vols., v. 103-106)<b><br>
         </b>--Provincial pamphlets, Allobroges - Touraine (41 pamphlets in 2 vols., 
-        v. 107-108)<o:p></o:p><br>
-        --Literary and Satirical Publications (89 pamphlets in 7 vols., v. 109-115)<o:p></o:p><br>
-        --Miscellaneous (34 pamphlets in 4 vols., v. 116-119)<o:p></o:p></span></font></p>
-      <p class=MsoNormal><font face="Arial, Helvetica, sans-serif"><span style='font-size:12.0pt'>As 
-        of June 2002, Euclid only contains these three entries for the entire 
+        v. 107-108)<br>
+        --Literary and Satirical Publications (89 pamphlets in 7 vols., v. 109-115)<br>
+        --Miscellaneous (34 pamphlets in 4 vols., v. 116-119)</p>
+      <p>As of June 2002, Euclid only contains these three entries for the entire 
         collection. More detailed access is provided by an analytical card catalogue 
-        and a chronological card catalogue, both in Special Collections.<o:p></o:p></span></font></p>
-      <p class=MsoNormal><font face="Arial, Helvetica, sans-serif"><span style='font-size:12.0pt'> 
-        <![if !supportEmptyParas]>
-        &nbsp; 
-        <![endif]>
-        <o:p></o:p></span></font></p>
-      <p class=MsoNormal><font face="Arial, Helvetica, sans-serif"><span style='font-size:12.0pt'> 
-        <![if !supportEmptyParas]>
-        &nbsp; 
-        <![endif]>
-        <o:p></o:p></span></font></p>
-      <p class=MsoNormal><font face="Arial, Helvetica, sans-serif"><span style='font-size:12.0pt'><b><a name="prov"></a>Provenance</b>:<o:p></o:p></span></font></p>
-      <p class=MsoNormal style='margin-right:-27.4pt'><font face="Arial, Helvetica, sans-serif"><span style='font-size:12.0pt'>The 
-        library purchased the collection in 1949.<span style="mso-spacerun: yes">&nbsp; 
-        </span>In January, 1949, one batch of 1500 was purchased from England.<span
-style="mso-spacerun: yes">&nbsp; </span>391 duplicates were sold to the Univ. 
-        of Alabama. (1)<span style="mso-spacerun: yes">&nbsp; </span>892 were 
-        catalogued as a group, 30 were catalogued individually.<span style="mso-spacerun:
-yes">&nbsp; </span>After January two more batches were purchased: a group of 300 
+        and a chronological card catalogue, both in Special Collections.</p>
+
+      <p><b><a name="prov"></a>Provenance</b>:</p>
+      <p>The library purchased the collection in 1949.
+      	In January, 1949, one batch of 1500 was purchased from England.
+	391 duplicates were sold to the Univ. 
+        of Alabama. (1)  892 were 
+        catalogued as a group, 30 were catalogued individually.
+	After January two more batches were purchased: a group of 300 
         titles and a group of 2,000+ (of this latter group, were some 500 duplicates???). 
-        (2)<span style="mso-spacerun: yes">&nbsp; </span>It appears, judging from 
+        (2) It appears, judging from 
         a bindery receipt found in volume 112, that the volumes were bound in 
-        Atlanta by the National Library Bindery Co. (2395 Peachtree Rd.).<o:p></o:p><o:p></o:p></span></font></p>
-      <p class=MsoNormal style='tab-stops:right 317.45pt'><font face="Arial, Helvetica, sans-serif"><span style='font-size:
-12.0pt'>1)They have catalogued some, if not all, of their collection on OCLC.<o:p></o:p><br>
+        Atlanta by the National Library Bindery Co. (2395 Peachtree Rd.).</p>
+      <p>1)They have catalogued some, if not all, of their collection on OCLC.<br>
         2)Emory Univ. Library Reports, (Special Collections) 1947-50, p. 114-15; 
-        XE302.A1<o:p></o:p></span></font></p>
-      <p class=MsoNormal><font face="Arial, Helvetica, sans-serif"><span style='font-size:12.0pt'> 
-        <![if !supportEmptyParas]>
-        &nbsp; 
-        <![endif]>
-        <o:p></o:p></span></font></p>
-      <p class=MsoNormal><font face="Arial, Helvetica, sans-serif"><span style='font-size:12.0pt'> 
-        <![if !supportEmptyParas]>
-        &nbsp; 
-        <![endif]>
-        <o:p></o:p></span></font></p>
-      <p class=MsoNormal><font face="Arial, Helvetica, sans-serif"><span style='font-size:12.0pt'><o:p></o:p></span></font></p>
-      <p class=MsoNormal><font face="Arial, Helvetica, sans-serif"><span style='font-size:12.0pt'><b><a name="link"></a>Links</b>:<o:p></o:p></span></font></p>
-      <p class=MsoNormal><font face="Arial, Helvetica, sans-serif"><span style='font-size:12.0pt'><a
-href="http://gallica.bnf.fr/">http://gallica.bnf.fr/</a><o:p></o:p><br>
-        The French National Library's digital site (Gallica) contains, besides a wealth of other material, a substantial number of pamphlets available in pdf format.<o:p></o:p><o:p></o:p></span></font></p>
-      <p class=MsoNormal><font face="Arial, Helvetica, sans-serif"><span style='font-size:12.0pt'><a
-href="http://humanities.uchicago.edu/orgs/ARTFL/">http://humanities.uchicago.edu/orgs/ARTFL/</a><o:p></o:p><br>
+        XE302.A1</p>
+
+	
+      <p><b><a name="link"></a>Links</b>:</p>
+      
+      <p><a href="http://gallica.bnf.fr/">http://gallica.bnf.fr/</a><br>
+	
+        The French National Library's digital site (Gallica) contains, besides a wealth of other material, a substantial number of pamphlets available in pdf format.</p>
+
+      <p><a href="http://humanities.uchicago.edu/orgs/ARTFL/">http://humanities.uchicago.edu/orgs/ARTFL/</a><br>
         The ARTFL site, maintained by the University of Chicago. A veritable treasure 
-        trove of digital material.<o:p></o:p></span></font></p>
-      <p class=MsoNormal><font face="Arial, Helvetica, sans-serif"><span style='font-size:12.0pt'><a
-href="http://humanities.uchicago.edu/orgs/ARTFL/projects/CRL/">http://humanities.uchicago.edu/orgs/ARTFL/projects/CRL/</a><o:p></o:p><br>
+        trove of digital material.</p>
+      <p><a href="http://humanities.uchicago.edu/orgs/ARTFL/projects/CRL/">http://humanities.uchicago.edu/orgs/ARTFL/projects/CRL/</a><br>
         ARTFL, in conjunction with CRL has put together a site dedicated to pamphlets 
-        from the 1848 revolution in France.<o:p></o:p><o:p></o:p></span></font></p>
-      <p class=MsoNormal><font face="Arial, Helvetica, sans-serif"><span style='font-size:12.0pt'><a
-href="http://web.library.emory.edu/subjects/humanities/history/FRindex.html">http://web.library.emory.edu/subjects/humanities/history/FRindex.html</a><o:p></o:p><br>
-        This links to Woodruff Library's Reference page for the French Revolution.<o:p></o:p></span></font><span style='font-size:12.0pt'><o:p></o:p></span></p>
+        from the 1848 revolution in France.</p>
+      <p><a href="http://web.library.emory.edu/subjects/humanities/history/FRindex.html">http://web.library.emory.edu/subjects/humanities/history/FRindex.html</a><br>
+        This links to Woodruff Library's Reference page for the French Revolution.</p>
 
 <?php
 include("footer.xml");
